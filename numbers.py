b02e1244894e1f786f2b97248b5424e8894cf071
nayan-s31 <nayansawant2005@gmail.com>
2026-06-11 10:49:56 +0530

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,12 @@
+# python/numbers.py
+
+numbers = [12, 45, 78, 23, 56, 89, 34, 67, 90, 11]
+
+highest = max(numbers)
+lowest = min(numbers)
+average = sum(numbers) / len(numbers)
+
+print("Numbers:", numbers)
+print("Highest:", highest)
+print("Lowest:", lowest)
+print("Average:", average)
